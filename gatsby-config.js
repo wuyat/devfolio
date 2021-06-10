1129d45c318e6938b0a59f8ff3e06f6889114d54
wuyat <wyattwilliamtravis@gmail.com>
2021-06-09 20:06:44 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,36 +3,36 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Wyatt Travis',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Wyatt Travis | Journalist`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Covers science, US/world news, and DC comics/superheros.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: ``,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: ,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: ``,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Previous History, English, and Classics teacher, and real estate agent. Current US/World news junkie, small business owner, and DC comics/Batman fan.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
+    Articles: [
       {
-        name: 'Devfolio',
+        name: 'The 10 Best Batman The Animated Series Episodes',
         description:
           'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Why The Snyderverse Should Not Be Restored',
         description:
           'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
         link: 'https://chromeextensionkit.com/?ref=devfolio',
       },
       {
-        name: 'Another Cool Project',
-        description:
+        name: 'What Warner Bros Gets Wrong About DC Comics,'
+        description: 
           'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
