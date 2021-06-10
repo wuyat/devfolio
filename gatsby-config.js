d7795539486a2ed7a5ad5aec344ac98a16415a67
wuyat <wyattwilliamtravis@gmail.com>
2021-06-09 20:11:26 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://wuyat.github.io/devfolio/`,
     // Your Name
     name: 'Wyatt Travis',
     // Main Site Title
